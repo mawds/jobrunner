e4d13e3ac104d36eead4b16f123428399ecbce0c
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-04-21 12:14:29 +0100

    Linting

--- jobrunner.py
+++ jobrunner.py
@@ -6,8 +6,7 @@ import itertools
 
 def parseGroup(groupstring):
     """Parse an extracted group; returns a list containg each option"""
-    m= re.search(r"^(\w):(.*)", groupstring)
-    
+    m = re.search(r"^(\w):(.*)", groupstring)
     if m:
         groupName = m.group(1)
         justgroups = m.group(2)
@@ -16,7 +15,7 @@ def parseGroup(groupstring):
         justgroups = groupstring 
 
     # If we've got an empty named group we don't want to overwrite it
-    if len(justgroups) > 0: 
+    if len(justgroups) > 0:
         groupList = justgroups.split(",")
     else:
         groupList = None
@@ -47,7 +46,7 @@ groupregex = re.compile(pattern)
 for group in groupregex.finditer(commandString):
     (groupName, groupValues) = parseGroup(group.group(1))
     if groupValues is not None:
-        groups[groupName] = groupValues 
+        groups[groupName] = groupValues
     groupList.append(groupName)
 
 
@@ -62,7 +61,9 @@ for i in itertools.product(*masterlist):
     thisCommand = commandString
     numSubs = 0
     while numSubs < len(groupList):
-        thisCommand = re.sub(pattern, i[masterlistnames.index(groupList[numSubs])], thisCommand, count=1)
+        thisCommand = re.sub(pattern, \
+        i[masterlistnames.index(groupList[numSubs])], \
+         thisCommand, count=1)
         numSubs = numSubs+1
     print thisCommand
 
